a832e77af9018ae69277a47ee812a9e284b76348
Kirill Medved <tankobaldoytpa@gmail.com>
2025-02-19 21:15:16 +0300

    refactor: replace all images in seeders to imgbb

--- server/seeders/20230402101838-sight.js
+++ server/seeders/20230402101838-sight.js
@@ -6,8 +6,7 @@ module.exports = {
     return queryInterface.bulkInsert("sights", [
       {
         name: "Brandenburg Gate",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/berlin/brandenburg-gate.jpg",
+        image: "https://i.ibb.co/4nM5s1PV/brandenburg-gate.png",
         description:
           "The Brandenburg Gate is a famous architectural monument located in the center of Berlin, at the eastern end of Pariser Platz. Like the Colosseum in Rome, Big Ben in London, St. Stephen's Cathedral in Vienna or the Eiffel Tower in Paris, the Brandenburg Gate has long been the trademark of Berlin, the symbol of the united Germany and one of the main tourist attractions of the German capital.",
         distance: "6.2 km",
@@ -18,8 +17,7 @@ module.exports = {
       },
       {
         name: "Kaiser Wilhelm Memorial Church",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/berlin/memorial-church.jpg",
+        image: "https://i.ibb.co/1JmpGjmP/kaiser-wilhelm-memorial-church.png",
         description:
           "The Kaiser Wilhelm Memorial Church is a famous landmark located in Berlin, Germany. The original church was built in the late 19th century in honor of Kaiser Wilhelm I, but was heavily damaged during World War II. The remains of the old church were preserved and a new modernist building was added next to it in the 1960s, creating a striking contrast between the old and the new. Today, the Kaiser Wilhelm Memorial Church serves as a memorial and a symbol of peace and reconciliation.",
         distance: "6.2 km",
@@ -30,8 +28,7 @@ module.exports = {
       },
       {
         name: "Checkpoint Charlie",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/berlin/check-point-charlie.jpg",
+        image: "https://i.ibb.co/RpRCn4Wq/checkpoint-charlie.png",
         description:
           "Checkpoint Charlie was a famous border crossing point in Berlin, Germany during the Cold War. It was located on the intersection of Friedrichstraße and Zimmerstraße and served as a crossing point for allied military personnel, diplomats, and foreigners between the Soviet-controlled East Berlin and the Western part of the city. The checkpoint was named after the third letter of the NATO phonetic alphabet, 'Charlie'. It became a symbol of the divide between East and West during the Cold War and was the site of several tense standoffs between Soviet and American forces. Today, a replica of the guardhouse and a small museum stand at the site to commemorate its historical significance.",
         distance: "6.2 km",
@@ -42,8 +39,7 @@ module.exports = {
       },
       {
         name: "Victory Column",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/berlin/victory-column.jpg",
+        image: "https://i.ibb.co/LDFn7qyC/victory-column.png",
         description:
           "The Victory Column, also known as the Berliner Siegessäule in German, is a famous monument located in Berlin, Germany. It was designed by Heinrich Strack and completed in 1873 to commemorate Prussia's victory over Denmark in the Second Schleswig War. Later, after Germany's unification, the monument was used to celebrate the country's military victories in the Austro-Prussian War and the Franco-Prussian War.",
         distance: "6.2 km",
@@ -54,8 +50,7 @@ module.exports = {
       },
       {
         name: "Fish market",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/hamburg/fischmarkt.jpg",
+        image: "https://i.ibb.co/Kz0HXDh8/fish-market.png",
         description:
           "The Fish Market in Hamburg, Germany is a famous open-air market that takes place every Sunday morning in the St. Pauli district, near the harbor. The market has a long history, dating back to the 16th century, and is a popular attraction for both locals and tourists.",
         distance: "6.2 km",
@@ -66,8 +61,7 @@ module.exports = {
       },
       {
         name: "International Maritime Museum",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/hamburg/international-maritime-museum.jpg",
+        image: "https://i.ibb.co/7tdXLv9m/international-maritime-museum.png",
         description:
           "The International Maritime Museum is located in Hamburg, Germany and is one of the largest maritime museums in the world. The museum is housed in an old brick warehouse in the historic Speicherstadt district, which was once the center of Hamburg's shipping industry.",
         distance: "6.2 km",
@@ -78,8 +72,7 @@ module.exports = {
       },
       {
         name: "Hagenbeck Zoo",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/hamburg/tierpark-hagenbeck.jpg",
+        image: "https://i.ibb.co/5hqyJBqv/hagenbeck-zoo.png",
         description:
           "The Hagenbeck Zoo, also known as Tierpark Hagenbeck, is a zoo located in Hamburg, Germany. The zoo was founded in 1907 by Carl Hagenbeck, a renowned animal trainer and dealer, and is known for its unique open-air enclosures that allow visitors to observe the animals in a natural habitat.",
         distance: "6.2 km",
@@ -90,8 +83,7 @@ module.exports = {
       },
       {
         name: "Dialogue in the Dark Museum",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/europe/germany/hamburg/dialogue-in-the-dark.jpg",
+        image: "https://i.ibb.co/j9JnYdRS/dialogue-dark-museum.png",
         description:
           "The Dialogue in the Dark Museum in Hamburg, Germany is a unique museum that offers visitors an immersive experience of what it's like to be blind. Visitors are led through a series of exhibits and interactive installations in complete darkness, accompanied by guides who are blind or visually impaired.",
         distance: "6.2 km",
@@ -102,8 +94,7 @@ module.exports = {
       },
       {
         name: "Minsk city hall",
-        image:
-          "https://cdn2.tu-tu.ru/image/pagetree_node_data/1/7626cf4c8cf8a3ae72b4f08befaed79d/",
+        image: "https://i.ibb.co/v67BdDMp/minsk-city-hall.png",
         description:
           "The Minsk City Hall is the seat of the local government in Minsk, the capital city of Belarus. It is located in the center of the city on Independence Square and serves as the headquarters for the Minsk City Executive Committee, which is responsible for governing the city.",
         distance: "6.2 km",
@@ -114,8 +105,7 @@ module.exports = {
       },
       {
         name: "Cathedral of the Blessed Virgin Mary",
-        image:
-          "https://cdn2.tu-tu.ru/image/pagetree_node_data/1/911233cfca0d4314998dc46a96ef4631/",
+        image: "https://i.ibb.co/cS6jyKqM/cathedral-blessed-virgin-mary.png",
         description:
           "The Cathedral of the Blessed Virgin Mary in Minsk, also known as the Minsk Cathedral, is a Catholic church located in the heart of Minsk, the capital city of Belarus. The cathedral was built in the 17th century in the Baroque style and is considered to be one of the most important landmarks in Minsk.",
         distance: "6.2 km",
@@ -126,8 +116,7 @@ module.exports = {
       },
       {
         name: "Gates of Minsk",
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Babrujskaja%2C_Minsk.jpg/300px-Babrujskaja%2C_Minsk.jpg",
+        image: "https://i.ibb.co/jkMsDffs/gates-minsk.png",
         description:
           "The Gates of Minsk, also known as the Minsk Gates, are a historic monument located in the capital city of Belarus, Minsk. The gates were built in the 17th century and are one of the few remaining architectural landmarks from the city's medieval period.",
         distance: "6.2 km",
@@ -138,8 +127,7 @@ module.exports = {
       },
       {
         name: "National Library of Belarus",
-        image:
-          "https://upload.wikimedia.org/wikipedia/ru/thumb/0/0e/BelarusNationalLibrary.JPG/290px-BelarusNationalLibrary.JPG",
+        image: "https://i.ibb.co/q6H4qgs/library-belarus.png",
         description:
           "The National Library of Belarus is the main library of the Republic of Belarus, located in the capital city of Minsk. It is one of the largest and most modern libraries in Europe and is known for its unique design and impressive collection of books and documents.",
         distance: "6.2 km",
@@ -150,8 +138,7 @@ module.exports = {
       },
       {
         name: "Old Castle",
-        image:
-          "https://avatars.dzeninfra.ru/get-zen_doc/4498748/pub_630374203fda8341457af3b8_63037469ee54bf127b727b7b/scale_1200",
+        image: "https://i.ibb.co/B5bq2ds8/old-castle.png",
         description:
           "The Old Castle in Grodno is a historic landmark located in the city of Grodno, Belarus. The castle was built in the 11th century and served as the residence of the Grand Dukes of Lithuania and the Kings of Poland for several centuries.",
         distance: "6.2 km",
@@ -162,8 +149,7 @@ module.exports = {
       },
       {
         name: "Great Choral Synagogue",
-        image:
-          "https://avatars.dzeninfra.ru/get-zen_doc/5236269/pub_630374203fda8341457af3b8_6303751756792f4c4b5bdd5f/scale_1200",
+        image: "https://i.ibb.co/fYNwN1dz/choral-synagogue.png",
         description:
           "The Great Choral Synagogue, also known as the New Synagogue, is a historic synagogue located in the city of Grodno, Belarus. The synagogue was built in the 19th century and is considered to be one of the most important landmarks of Jewish heritage in the country.",
         distance: "6.2 km",
@@ -174,8 +160,7 @@ module.exports = {
       },
       {
         name: "Grodno State Museum of the History of Religion",
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Museum_of_religion.JPG/274px-Museum_of_religion.JPG",
+        image: "https://i.ibb.co/k2nfGRZV/museum-history-religion.png",
         description:
           "The Grodno State Museum of the History of Religion is a museum located in the city of Grodno, Belarus. The museum was established in 1994 and is dedicated to the study and preservation of the history of religion in Belarus.",
         distance: "6.2 km",
@@ -186,8 +171,7 @@ module.exports = {
       },
       {
         name: "Zhiliber Park",
-        image:
-          "https://trippo.ru/wp-content/uploads/2021/12/Park-ZHilibera.jpg",
+        image: "https://i.ibb.co/bRzmd8H0/zhiliber-park.png",
         description:
           "Zhiliber Park is a public park located in the city of Grodno, Belarus. The park is situated on the banks of the Neman River and covers an area of over 12 hectares. It is considered to be one of the most popular and scenic parks in Grodno.",
         distance: "6.2 km",
@@ -198,8 +182,7 @@ module.exports = {
       },
       {
         name: "Mugni Church",
-        image:
-          "https://blogtravel.by/wp-content/uploads/2021/10/dostoprimechatelnosty-yerevana-34.jpg",
+        image: "https://i.ibb.co/3mmPGMBj/mugni-church.png",
         description:
           "The Holy Mother of God Katoghike Church, commonly known as the Mugni Church, is a historic Armenian Apostolic church located in the heart of Yerevan, the capital city of Armenia. The church was built in the 17th century and is considered to be one of the most important landmarks of Armenian culture and heritage in the city.",
         distance: "6.2 km",
@@ -210,8 +193,7 @@ module.exports = {
       },
       {
         name: "Arjeni Winery",
-        image:
-          "https://blogtravel.by/wp-content/uploads/2021/10/dostoprimechatelnosty-yerevana-44.jpg",
+        image: "https://i.ibb.co/1tygWL7B/arjeni-winery.png",
         description:
           "The Ararat-Echmiadzin Winery, also known as the Arjeni Winery, is a winery located in Yerevan, the capital city of Armenia. The winery was established in 1953 and is one of the oldest and most famous wineries in Armenia.",
         distance: "6.2 km",
@@ -222,8 +204,7 @@ module.exports = {
       },
       {
         name: "Republic Square",
-        image:
-          "https://blogtravel.by/wp-content/uploads/2021/10/dostoprimechatelnosty-yerevana-1.jpg",
+        image: "https://i.ibb.co/YTWrynSJ/republic-square.png",
         description:
           "Republic Square is a central public square located in the heart of Yerevan, the capital city of Armenia. The square is one of the main landmarks of the city and is a popular destination for tourists and locals alike.",
         distance: "6.2 km",
@@ -234,8 +215,7 @@ module.exports = {
       },
       {
         name: "Yerevan Cascade",
-        image:
-          "https://blogtravel.by/wp-content/uploads/2021/10/dostoprimechatelnosty-yerevana-3.jpg",
+        image: "https://i.ibb.co/B2mSgn12/yerevan-cascade.png",
         description:
           "The Yerevan Cascade is a massive staircase complex located in the capital city of Armenia, Yerevan. The Cascade is an architectural masterpiece that consists of a series of outdoor escalators and staircases, which lead up to a large plaza at the top of the complex.",
         distance: "6.2 km",
@@ -246,8 +226,7 @@ module.exports = {
       },
       {
         name: "Black Fortress",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/asia/armenia/gyumri/black-fortress.jpg",
+        image: "https://i.ibb.co/7thnkj3S/black-fortress.png",
         distance: "6.2 km",
         price: "8$",
         CityId: 6,
@@ -258,8 +237,7 @@ module.exports = {
       },
       {
         name: "Vardanants Square",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/asia/armenia/gyumri/vardanants-square.jpg",
+        image: "https://i.ibb.co/cKJ8FJRy/vardanants-square.png",
         description:
           "Vardanants Square is the central square of Gyumri, the second-largest city in Armenia. The square is named after the Battle of Avarayr, which took place in 451 AD and is considered a significant event in Armenian history.",
         distance: "6.2 km",
@@ -270,8 +248,7 @@ module.exports = {
       },
       {
         name: "Central Park",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/asia/armenia/gyumri/central-park.jpg",
+        image: "https://i.ibb.co/YFc5PpcD/central-park.png",
         description:
           "Central Park is a public park located in the heart of Gyumri, the second-largest city in Armenia. The park is a popular destination for locals and visitors alike, offering a peaceful retreat from the hustle and bustle of city life.",
         distance: "6.2 km",
@@ -282,8 +259,7 @@ module.exports = {
       },
       {
         name: "Marmashen Monastery",
-        image:
-          "https://tripplanet.ru/wp-content/uploads/asia/armenia/gyumri/marmashen-monastery.jpg",
+        image: "https://i.ibb.co/nN79DqWH/marmashen-monastery.png",
         description:
           "Marmashen Monastery is a historic Armenian monastery located near the city of Gyumri in the Shirak region of Armenia. The monastery was built in the 10th century and is considered one of the finest examples of Armenian medieval architecture.",
         distance: "6.2 km",
--- server/seeders/20230402101841-guide.js
+++ server/seeders/20230402101841-guide.js
@@ -6,84 +6,73 @@ module.exports = {
     return queryInterface.bulkInsert("guides", [
       {
         name: "Andrey Medvedik",
-        image:
-          "https://sun9-46.userapi.com/impg/bDcJoqmnwZpzwLc-IECcREcYS0B89VXEKyMzxQ/0GKr3f4JB8A.jpg?size=400x400&quality=95&sign=51bc517f02df15bf197c5b20c84701a5&type=album",
+        image: "https://i.ibb.co/Nd9MBTwd/andr.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Stepan Naymkin",
-        image:
-          "https://sun9-80.userapi.com/impg/Pt7HWGkk_FAGTlDI5i6nJIb72Qq9xzHmWVgtVw/1M24R8bUAhk.jpg?size=1200x1600&quality=96&sign=227d56fceee4e0deff1b268c0cdb65d8&type=album",
+        image: "https://i.ibb.co/rKfg9BX0/stepank.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Anna Kravchenko",
-        image:
-          "https://mobimg.b-cdn.net/v3/fetch/ea/eaa626bfeedf4b90cb715b2c5b311477.jpeg",
+        image: "https://i.ibb.co/7Jqt17cm/kravchenko.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Olga Levchenko",
-        image:
-          "https://get.pxhere.com/photo/man-person-people-woman-photography-male-portrait-spring-facial-expression-smile-senior-citizen-cool-image-eye-glasses-head-skin-beauty-emotion-portrait-photography-vision-care-209032.jpg",
+        image: "https://i.ibb.co/hxD5KqZD/levchenko.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Maria Schmidt",
-        image:
-          "https://get.pxhere.com/photo/person-people-girl-woman-hair-photography-portrait-model-spring-child-lady-facial-expression-season-smile-face-dress-eye-head-skin-beauty-blond-emotion-photo-shoot-brown-hair-portrait-photography-151299.jpg",
+        image: "https://i.ibb.co/Rk9B1pMM/schmidt.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Jan Mueller",
-        image: "http://cdn1.flamp.ru/a992cfb02dd71b2dc22b2f577067ddd8.jpg",
+        image: "https://i.ibb.co/YTK1PnZM/mueller.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Lena Wagner",
-        image:
-          "https://media.fulledu.ru/documents/images/1k27/5b5cc9b1d6e4a901de7bb012/article5b5cc9b16f8be4.35382404.jpg",
+        image: "https://i.ibb.co/gHRCTN6/wagner.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Max Schulz",
-        image:
-          "https://psychologie-seminar.com/____impro/1/onewebmedia/Young%20happy%20man%20smiling%20at%20camera%20in%20the%20grape%20fields.jpg?etag=&sourceContentType=&ignoreAspectRatio&resize=1200%2B800",
+        image: "https://i.ibb.co/cc9f3X4H/schulz.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Mark Arutyunyan",
-        image:
-          "https://get.pxhere.com/photo/man-person-hair-male-sadness-portrait-human-hairstyle-beard-close-up-face-loneliness-eye-head-stand-alone-portrait-photography-facial-hair-aycuma-1363432.jpg",
+        image: "https://i.ibb.co/q35kQzyd/arutyunyan.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Artur Avetisyan",
-        image:
-          "https://get.pxhere.com/photo/man-nature-person-people-male-portrait-human-close-smile-senior-citizen-face-head-out-grandparent-facial-hair-720070.jpg",
+        image: "https://i.ibb.co/Tx7KHtk2/avetisyan.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Narek Arutyunyan",
-        image:
-          "https://mysekret.ru/wp-content/uploads/2022/01/warren-wong-vvewjjrrhgk-unsplash.jpg",
+        image: "https://i.ibb.co/tfpQnyB/arutyunyan.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         name: "Angela Abramyan",
-        image:
-          "https://get.pxhere.com/photo/person-people-girl-woman-photography-portrait-young-youth-student-healthy-facial-expression-smiling-smile-teenager-caucasian-beauty-teeth-teen-attractive-emotion-photo-shoot-brown-hair-portrait-photography-1391382.jpg",
+        image: "https://i.ibb.co/Kcqv7r2S/abramyan.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
