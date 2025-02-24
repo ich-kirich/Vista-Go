952942a36ad61e20801cfd9a2d15bc89e48ece4c
Kirill Medved <tankobaldoytpa@gmail.com>
2025-02-24 10:32:39 +0300

    feat: add .env.example

--- client/src/API/axiosConfig.ts
+++ client/src/API/axiosConfig.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 export const adminHost = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 const authInterceptor = (config: any) => {
